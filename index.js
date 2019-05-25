9287b7b176ad01a5a10319f3b53d16dd16a3c5b5
jeffersondesouza <jeffersondesouzacordel@gmail.com>
2019-05-25 18:57:31 -0300

    init first exercise

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('hello q');
+const columbian = {
+  name: "columbian",
+  basePrice: 5
+};
+
+const frenchRoast = {
+  name: "french roast",
+  basePrice: 8
+};
+
+const decaf = {
+  name: "decaf",
+  basePrice: 6
+};
